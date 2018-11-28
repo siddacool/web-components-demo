c04c3ccc8696eecd646ceb2b1b562b044cfc4282
Siddhesh Mangela <sidthecool007@gmail.com>
2018-11-28 15:28:11 +0530

    pwa path fixed

---
+++ src/components/WebcomponentDempApp.js
@@ -0,0 +1,7 @@
+class WebcomponentDempApp extends HTMLElement {
+  constructor() {
+    super('webcomponent-demp-app');
+  }
+}
+
+customElements.define('webcomponent-demp-app', WebcomponentDempApp);
